5e05f71bebb1a340395fd6d7ef6b242c504a2f5e
Vu Quoc Phong <phong.vq.198@gmail.com>
2021-08-01 18:13:08 +0700

    remove all tf code

--- config/terraform/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "remote" {
-    organization = "emyeukhoahoc"
-
-    workspaces {
-      name = "<%= expansion('dailycookies-tfc-:ENV') %>"
-    }
-  }
-}
--- config/terraform/provider.tf
+++
@@ -1,9 +0,0 @@
-#  Docs: https://www.terraform.io/docs/providers/aws/index.html
-
-#  If AWS_PROFILE and AWS_REGION is set, then the provider is optional.  Here's an example anyway:
-
-provider "aws" {
-  region  = "<%= ENV["AWS_REGION"] %>"
-  access_key  = "<%= ENV["AWS_ACCESS_KEY_ID"] %>"
-  secret_key  = "<%= ENV["AWS_SECRET_ACCESS_KEY"] %>"
-}
