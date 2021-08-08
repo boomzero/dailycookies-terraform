6b22bca0df48953ea266d9089f7aa3e4fb698647
Vu Quoc Phong <phong.vq.198@gmail.com>
2021-08-08 16:25:46 +0700

    \#2 add backend remote state (tfc) configuration

--- provider.tf
+++ provider.tf
@@ -2,3 +2,13 @@ provider "aws" {
   region = var.region
 }
 
+terraform {
+  backend "remote" {
+    hostname = "app.terraform.io"
+    organization = "emyeukhoahoc"
+
+    workspaces {
+      prefix = "dailycookies-tfc-"
+    }
+  }
+}
