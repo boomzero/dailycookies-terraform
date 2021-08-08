c190954a2a2052f6fbc091b629da5c8df2828ca1
Vu Quoc Phong <phong.vq.198@gmail.com>
2021-08-08 15:07:45 +0700

    #2 re-organize locals constants

--- locals.tf
+++ locals.tf
@@ -1,9 +1,19 @@
 locals {
   # naming convention: ec2-${region}-${short_env}-${app_code}
-  short_env                                    = substr(var.env, 0, 1)
-  core_vpc_name                                = "vpc-${var.region}-${local.short_env}-core-stack"
-  internet_gateway_vpc_name                    = "igw-${var.region}-${local.short_env}-core-stack"
-  core_public_subnet_name                      = "subnet-${var.region}-${local.short_env}-core-stack-public"
+  short_env = substr(var.env, 0, 1)
+
+  # vpc/subnet/gateway
+  core_vpc_name             = "vpc-${var.region}-${local.short_env}-core-stack"
+  internet_gateway_vpc_name = "igw-${var.region}-${local.short_env}-core-stack"
+  core_public_subnet_name   = "subnet-${var.region}-${local.short_env}-core-stack-public"
+
+  # routing
+  core_public_rtb_name = "rtb-${var.region}-${var.short_env}-core-stack-public"
+
+  # network access
+  core_stack_webserver_security_group_name = "sg-${var.region}-${var.short_env}-core-stack-webserver"
+
+  # compute
   webserver_core_public_network_interface_name = "interface-${var.region}-${local.short_env}-core-stack-public-webserver"
   webserver_instance_name                      = "ec2-${var.region}-${local.short_env}-webserver"
 }
