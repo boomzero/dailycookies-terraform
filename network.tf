1eafc6770003480fa94af64304b6797179e4144c
Vu Quoc Phong <phong.vq.198@gmail.com>
2021-08-08 15:08:06 +0700

    #2 add security group, route table, igw

--- network.tf
+++ network.tf
@@ -1,3 +1,6 @@
+/* -------------------------------------------------------------------------- */
+/*                               # VPC / subnet                               */
+/* -------------------------------------------------------------------------- */
 resource "aws_vpc" "core_vpc" {
   cidr_block = var.core_vpc_cidr
 
@@ -25,10 +28,50 @@ resource "aws_subnet" "core_public" {
   }
 }
 
+/* -------------------------------------------------------------------------- */
+/*                             Network interfaces                             */
+/* -------------------------------------------------------------------------- */
 resource "aws_network_interface" "core_public_webserver" {
   subnet_id = aws_subnet.core_public.id
 
   tags = {
     Name = local.webserver_core_public_network_interface_name
   }
+}
+
+/* -------------------------------------------------------------------------- */
+/*                                   Routing                                  */
+/* -------------------------------------------------------------------------- */
+resource "aws_route_table" "core_public" {
+  vpc_id = aws_vpc.core_vpc.id
+
+  route = [
+    {
+      cidr_block = "0.0.0.0/0"
+      gateway_id = aws_internet_gateway.core_stack.id
+    }
+  ]
+
+  tags = {
+    Name = local.core_public_rtb_name
+  }
+}
+
+resource "aws_route_table_association" "core_public" {
+  subnet_id      = aws_subnet.core_public.id
+  route_table_id = aws_route_table.core_public.id
+}
+
+/* -------------------------------------------------------------------------- */
+/*                               Network Access                               */
+/* -------------------------------------------------------------------------- */
+module "core_stack_webserver_sg" {
+  source = "terraform-aws-modules/security-group/aws"
+
+  name        = local.core_stack_webserver_security_group_name
+  description = "Allow HTTP/HTTPS, SSH access to webserver from internet"
+  vpc_id      = aws_vpc.core_vpc.id
+
+  ingress_cidr_blocks      = ["0.0.0.0/0"]
+  ingress_rules            = ["https-443-tcp", "http-80-tcp", "ssh-tcp"]
 }
